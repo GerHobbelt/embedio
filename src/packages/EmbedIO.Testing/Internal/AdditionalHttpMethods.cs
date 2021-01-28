ba1aa958ddf9cc3eb31db70588cd262217f91d46
Ger Hobbelt <ger@hobbelt.com>
2021-01-28 16:21:55 +0100

    using the latest .NET frameworks: https://docs.microsoft.com/en-us/dotnet/standard/frameworks#latest-versions

--- src/packages/EmbedIO.Testing/Internal/AdditionalHttpMethods.cs
+++ src/packages/EmbedIO.Testing/Internal/AdditionalHttpMethods.cs
@@ -2,7 +2,7 @@
 
 namespace EmbedIO.Testing.Internal
 {
-#if NETSTANDARD2_0
+#if NETSTANDARD2_0 || NETSTANDARD2_1
     internal static class AdditionalHttpMethods
     {
         public static readonly HttpMethod Patch = new HttpMethod("PATCH");
